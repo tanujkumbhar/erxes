49e33e38b3721b8fb76106249aca9754164db522
Dulguun Otgon <dulguun0225@gmail.com>
2023-04-20 21:18:54 +0800

    try exec instead of execSync

--- packages/gateway/src/apollo-router/supergraph-compose.ts
+++ packages/gateway/src/apollo-router/supergraph-compose.ts
@@ -4,9 +4,12 @@ dotenv.config();
 import { ErxesProxyTarget } from 'src/proxy/targets';
 import { supergraphConfigPath, supergraphPath } from './paths';
 import * as fs from 'fs';
-import { execSync } from 'child_process';
+import { execSync, exec as execCb } from 'child_process';
 import isSameFile from '../util/is-same-file';
 import * as yaml from 'yaml';
+import { promisify } from 'util';
+
+const exec = promisify(execCb);
 
 const { NODE_ENV, SUPERGRAPH_POLL_INTERVAL_MS } = process.env;
 
@@ -53,15 +56,15 @@ const createSupergraphConfig = (proxyTargets: ErxesProxyTarget[]) => {
 const supergraphComposeOnce = async () => {
   if (NODE_ENV === 'production') {
     // Don't rewrite supergraph schema if it exists. Delete and restart to update the supergraph.graphql
-    if (fs.existsSync(supergraphPath)) {
-      return;
-    }
-    execSync(
-      `rover supergraph compose --config ${supergraphConfigPath} --output ${supergraphPath} --elv2-license=accept`,
-      {
-        stdio: 'inherit'
-      }
+    // if (fs.existsSync(supergraphPath)) {
+    //   return;
+    // }
+    const { stdout, stderr } = await exec(
+      `rover supergraph compose --config ${supergraphConfigPath} --output ${supergraphPath} --elv2-license=accept`
     );
+
+    if (stdout) console.log(stdout);
+    if (stderr) throw new Error(stderr);
   } else {
     const superGraphqlNext = supergraphPath + '.next';
     execSync(
