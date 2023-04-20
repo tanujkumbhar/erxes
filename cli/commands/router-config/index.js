30a4e7c23303dde7f8cc85627e511f1471b9963b
Dulguun Otgon <dulguun0225@gmail.com>
2023-04-20 15:38:53 +0800

    router.yaml and rhai

--- cli/commands/router-config/index.js
+++ cli/commands/router-config/index.js
@@ -4,6 +4,7 @@ const yaml = require('yaml');
 const { log, execCommand, filePath, execCurl } = require('../utils');
 const { generateLBaddress } = require('../docker/utils');
 const { execSync } = require('child_process');
+const path = require('path');
 
 async function supergraphYaml(configs, isDev) {
 let port = 3300;
@@ -49,9 +50,54 @@ async function supergraph(configs, isDev) {
   await supergraphGraphql(configs, isDev);
 }
 
-async function config(configs) {}
+async function config(configs) {
+  const widgets = configs.widgets || {};
+  const WIDGETS_DOMAIN = widgets.domain || `${configs.domain}/widgets`;
+  const CLIENT_PORTAL_DOMAINS = configs.client_portal_domains || '';
+  const ALLOWED_ORIGINS = configs.allowed_origins;
+  const { DOMAIN } = configs || {};
 
-async function rhai() {}
+  const config = {
+    include_subgraph_errors: {
+      all: true
+    },
+    rhai: {
+      main: "/dist/config/rhai/main.rhai"
+    },
+    cors: {
+      allow_credentials: true,
+      origins: [
+        DOMAIN ? DOMAIN : 'http://localhost:3000',
+        WIDGETS_DOMAIN ? WIDGETS_DOMAIN : 'http://localhost:3200',
+        ...(CLIENT_PORTAL_DOMAINS || '').split(','),
+        'https://studio.apollographql.com'
+      ].filter(x => typeof x === 'string'),
+      match_origins: (ALLOWED_ORIGINS || '').split(',').filter(Boolean)
+    },
+    headers: {
+      all: {
+        request: [
+          {
+            propagate: {
+              matching: '.*'
+            }
+          }
+        ]
+      }
+    },
+    supergraph: {
+      listen: '0.0.0.0:4000'
+    }
+  };
+
+  fs.writeFileSync(`${configs.router_config_dir}/router.yaml`, yaml.stringify(config));
+}
+
+async function rhai(configs) {
+  const rhaiDir = path.resolve(__dirname, 'rhai');
+  const targetDir = `${configs.router_config_dir}/rhai`;
+  await fse.copydir(rhaiDir, targetDir);
+}
 
 async function generate(program) {
   const isDev = program && program.dev;
@@ -59,7 +105,7 @@ async function generate(program) {
 
   await supergraph(configs, isDev);
   await config(configs, isDev);
-  await rhai();
+  await rhai(configs);
 }
 
 module.exports = {
